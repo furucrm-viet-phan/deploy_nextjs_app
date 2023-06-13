7c20627c3d226b988a81d491b01c4a90667a29eb
furucrm-viet-phan <viet.phan@furucrm.com>
2023-06-13 16:02:21 +0700

    Change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,16 @@ export NVM_DIR=$HOME/.nvm;
 . $NVM_DIR/nvm.sh;
 
 cd /home/ec2-user/projects/deploy_nextjs_app
-git pull origin main
+
+# Check the current branch and pull from the appropriate branch
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+if [ "$current_branch" == "production" ]; then
+  git pull origin production
+else
+  git pull origin main
+fi
+
 nvm use v16
 yarn install &&
 yarn build &&
